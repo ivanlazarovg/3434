2de31c5b10c71911c0200cf51a9332901f19d572
Kris-Dimitrov <k31122004@gmail.com>
2026-03-23 22:53:49 +0100

    Added Movement

---
+++ Assets/Scripts/PlayerCameraController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PlayerCameraController : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed;
+    [SerializeField] float minYRotation;
+    [SerializeField] float maxYRotation;
+
+    GameObject CameraHolder;
+    Vector3 addedRotationCam;
+    Vector3 addedRotation;
+
+
+
+    void Start()
+    {
+        CameraHolder = this.gameObject.transform.GetChild(0).gameObject;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        addedRotation = Vector3.zero;
+        addedRotationCam = Vector3.zero;
+
+        addedRotation += Vector3.up * Input.GetAxis("Mouse X") * rotationSpeed;
+        addedRotationCam += Vector3.left * Input.GetAxis("Mouse Y") * rotationSpeed;
+
+        Debug.Log(addedRotation);
+        Debug.Log(addedRotationCam);
+
+        this.transform.eulerAngles += addedRotation * Time.deltaTime;
+        CameraHolder.transform.eulerAngles += addedRotationCam * Time.deltaTime;
+
+    }
+}
