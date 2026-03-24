cede3d1119bb3cee0ad8511f8910adb1020b7745
ivanlazarovg <43958429+ivanlazarovg@users.noreply.github.com>
2026-03-24 04:41:59 +0200

    cum

--- Assets/Scripts/CumMeterDisplay.cs
+++ Assets/Scripts/CumMeterDisplay.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CumMeterDisplay : MonoBehaviour
 {
+    public Slider cumSlider;
+
     public void DisplayPointsGained()
     {
         StartCoroutine(PopAndShrink());
@@ -12,5 +15,10 @@ public class CumMeterDisplay : MonoBehaviour
     private IEnumerator PopAndShrink()
     {
         yield return null;
+    }
+
+    public void SetSliderValue(float valueToSet)
+    {
+        cumSlider.value = valueToSet;
     }
 }
--- Assets/Scripts/CumPointsManager.cs
+++ Assets/Scripts/CumPointsManager.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,16 @@ using UnityEngine;
 public class CumPointsManager : MonoBehaviour
 {
     CumMeterDisplay cumDisplay;
+
+    float decayTimer = 0;
+
+    public int decayThreshold;
+
+    public float decayRate;
+
+    public float scoreModifier = 0.001f;
+
+    private float score = 0;
     void Start()
     {
         cumDisplay = FindAnyObjectByType<CumMeterDisplay>();
@@ -13,11 +24,41 @@ public class CumPointsManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            IncreasePoints(Random.Range(100, 500));
+        }
+
+        decayTimer += Time.deltaTime;
+
+        if (decayTimer > 1)
+        {
+            DecayPoints();
+        }
     }
 
-    public void IncreasePoints (int pointsIncrease)
+    public void IncreasePoints(int pointsIncrease)
     {
         cumDisplay.DisplayPointsGained();
+
+        if (pointsIncrease > decayThreshold)
+        {
+            decayTimer = 0;
+        }
+
+        score += pointsIncrease * scoreModifier;
+
+        cumDisplay.SetSliderValue(score);
+
+    }
+
+    public void DecayPoints()
+    {
+        cumDisplay.DisplayPointsGained();
+
+        score -= decayRate * Time.deltaTime;
+
+        cumDisplay.SetSliderValue(score);
+
     }
 }
