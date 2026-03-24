168e0bda65f83d3e5881ae5442bfaff0d785da0d
ivanlazarovg <43958429+ivanlazarovg@users.noreply.github.com>
2026-03-24 18:09:43 +0200

    hard kur

--- Assets/Scripts/PowerUpPickUp.cs
+++ Assets/Scripts/PowerUpPickUp.cs
@@ -6,6 +6,9 @@ public class PowerUpPickUp : MonoBehaviour
 {
     public PowerUp powerUpToPickUp;
 
+    float rotationTime = 0;
+    float sinTime = 0;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
@@ -13,5 +16,14 @@ public class PowerUpPickUp : MonoBehaviour
             PowerUpManager.Instance.SetActivePowerUp(powerUpToPickUp);
             Destroy(gameObject);
         }
+    }
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up, 20 * Time.deltaTime);
+
+        sinTime += Time.deltaTime * 2;
+
+        transform.position = new Vector3(transform.position.x, Mathf.Sin(sinTime) * 0.5f, transform.position.z);
     }
 }
