33cb93f28c230595c93d6d91100f9a76943673b1
Ivan Lazarov <74430991+Kris-Dimitrov@users.noreply.github.com>
2026-03-24 23:08:17 +0100

    kurec

--- Assets/Scripts/EffectApplier.cs
+++ Assets/Scripts/EffectApplier.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[ExecuteInEditMode]
 
 public class EffectApplier : MonoBehaviour
 {
     [Range(1,16)]
     [SerializeField] float resolutionChange;
+    [SerializeField] Material effect;
+
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        RenderTexture temp = new RenderTexture((int)(source.width /resolutionChange), (int)(source.height / resolutionChange), 0);
+        RenderTexture temp = new RenderTexture((int)(source.width / resolutionChange), (int)(source.height / resolutionChange), 0);
         temp.filterMode = FilterMode.Point;
 
-        Graphics.Blit(source, temp);
+        if (effect != null) Graphics.Blit(source, temp, effect);
+        else Graphics.Blit(source, temp);
         Graphics.Blit(temp, destination);
 
         temp.Release();
